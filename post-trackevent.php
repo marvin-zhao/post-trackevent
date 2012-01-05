a8e503e804527f2fce3bc828c7d01fe514eccb7e
joysboy zyxf <zyxfsky@gmail.com>
2012-01-06 05:21:19 +0800

    add Description

--- post-trackevent.php
+++ post-trackevent.php
@@ -2,10 +2,10 @@
 /*
 Plugin Name: Add Post TrackEvent
 Plugin URI: http://xfeng.me/add-post-trackevent-wordpress-plugin/
-Description: This plugin automatically add the onclick="_gaq.push(['_trackEvent', 'category', 'action', 'URL']);" to link in a post. \n You can modify category and action that you like. You can choose opt_label as domain or url.
+Description: This plugin automatically add the onclick="_gaq.push(['_trackEvent', 'category', 'action', 'URL']);" to link in a post.为你的博客文章内链接加上Google Analytics事件跟踪，你可以自己修改category和action名字. 你可以选择注释插件中的正则,切换事件跟踪标签为完整的外链地址或仅为外链域名.
 Author: JoysBoy 
 Author URI: http://xfeng.me
-Version: 0.1
+Version: 0.2
 Put in /wp-content/plugins/ of your Wordpress installation
 */
 add_filter('the_content', 'add_post_trackevent_replace');
